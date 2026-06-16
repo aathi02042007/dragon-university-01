fd32b5691f460e55016eeedb10cd4ee1c627b5d7
aathi02042007 <subanya0204@gmail.com>
2026-06-16 15:23:59 +0530

    Revert "Test"

--- app/core/config.py
+++ app/core/config.py
@@ -1,12 +1,2 @@
-﻿from pydantic_settings import BaseSettings
+﻿# TODO: Configure application settings and env reading
 
-class settings(BaseSettings):
-    DATABASE_URL:str
-    SECRET_KEY:str
-    ALGORITHM:str
-    ACCESS_TOKEN_EXPIRE_MINUTES:int
-
-    class Config:
-        env_file=".env"
-
-settings=settings()
--- app/database/session.py
+++ app/database/session.py
@@ -1,19 +1,2 @@
-﻿from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+﻿# TODO: Configure SQLAlchemy db engine and sessionmaker
 
-from app.core.config import settings
-
-engine = create_engine(settings.DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
